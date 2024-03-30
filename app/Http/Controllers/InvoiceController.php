9e9d787160ed2e642869f56f050daec48f7a8b4e
Srikanth Janjirala <srikanth.j@crestinfosystems.net>
2024-03-30 11:12:57 +0530

    list all the invoices

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Invoice;
+use App\Models\InvoiceItem;
 
 class InvoiceController extends Controller
 {
-    //
+
+    public function get_all_invoice()
+    {
+        $invoices = Invoice::with(['customer'])->orderBy('id','DESC')->get();
+
+        return response()->json([
+            'invoices'  => $invoices
+        ]);
+    }
 }
